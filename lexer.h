01d05f0604ce516b0709694a318ebf1e7dcadd0d
GerardoBrescia <73852292+GerardoBrescia@users.noreply.github.com>
2022-05-31 10:02:33 +0200

    Delete lexer.h

--- lexer.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include<ctype.h>
-#include <glib.h>
-
-
-#include "file_chunk.h"
-#include "word.h"
-
-Word_occurrence * get_lexeme_from_chunk(File_chunk *chunks_received, int chunk_number, int rank, int * num_word);
